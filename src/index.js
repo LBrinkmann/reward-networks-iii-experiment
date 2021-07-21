990d5b258296723462f6cb6c226f3ef91fd41c00
LBrinkmann <com@levin-brinkmann.de>
2021-07-21 20:45:49 +0200

    Init frontend.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import config from "./config";
+import axios from "axios";
 
 function App() {
-  const [count, set_count] = useState(0);
+  const [game, set_game] = useState();
+  const [solutions, set_solutions] = useState([]);
+  const [environment, set_environment] = useState();
+  const [pervSolutions, set_prev_solutions] = useState();
+  const [stage, set_stage] = useState("loading");
 
-  function on_click() {
-    set_count(count + 1);
-  }
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(`${config.backend_url}/game/test`);
+
+      console.log(result);
+
+      if (result.userExist) {
+        set_stage("userExists");
+      } else {
+        set_game(result.game);
+        set_solutions(result.solutions);
+        set_environment(result.environment);
+        set_prev_solutions(result.pervSolutions);
+        set_stage("intro");
+      }
+    }
+    fetchData();
+  }, []);
+
+  //   useEffect(async () => {
+  //     if (solutions.length == 5) {
+  //       const requestOptions = {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify({ solutions, game }),
+  //       };
+  //       const response = await fetch(
+  //         `${config.backend_url}/solutions`,
+  //         requestOptions
+  //       );
+  //       const data = await response.json();
+  //       set_stage("outro");
+  //     }
+  //   }, [solutions]);
 
   return (
-    <div style={{margin: "2em"}}>
-      <p><button onClick={on_click}>Click Me</button></p>
+    <div style={{ margin: "2em" }}>
+      <p>{game}</p>
+      <p>{solutions}</p>
+      <p>{environment}</p>
+      <p>{pervSolutions}</p>
 
-      <p>You clicked {count} times.</p>
-
-      <p>Backend URL: <code>{config.backend_url}</code></p>
-
+      <p>
+        Backend URL: <code>{config.backend_url}</code>
+      </p>
     </div>
   );
 }
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,32 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "postcss-loader"
-        ]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "static/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/",
-    hot: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+    entry: ["babel-polyfill", "./src/index.js"],
+    mode: "development",
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+        ],
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/",
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "static/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/",
+        hot: true,
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
 };
