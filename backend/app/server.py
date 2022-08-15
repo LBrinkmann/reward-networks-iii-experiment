2d3f002e63a4952c3274b74b30a6ec2ab123d339
vagechirkov <vagechirkov@gmail.com>
2022-08-15 21:15:16 +0200

    refactor server file

--- backend/app/server.py
+++ backend/app/server.py
@@ -3,18 +3,18 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .experiment.utils import load_experiments
-from .models.experiment import Experiment
-from .models.utils import reset_all
-from .routes.advise import advise_router
-from .routes.experiment import experiment_router
-from .routes.results import results_router
-from .routes.session import session_router
+from database.connection import Settings
+from routes.advise import advise_router
+from routes.experiment import experiment_router
+from routes.results import results_router
+from routes.session import session_router
 
 if os.getenv('GENERATE_FRONTEND_TYPES', default='false') == 'true':
     from pydantic2ts import generate_typescript_defs
 
 api = FastAPI()
+
+settings = Settings()
 
 api.add_middleware(
     CORSMiddleware,
@@ -30,10 +30,13 @@ api.include_router(advise_router, prefix="/advise")
 api.include_router(experiment_router, prefix="/experiment")
 api.include_router(results_router, prefix="")
 
+
 @api.on_event("startup")
 async def startup_event():
-    reset_all()
-    load_experiments('./app/data/experiments.yml')
+    # initialize database
+    await settings.initialize_database()
+
+    # generate frontend types
     if os.getenv('GENERATE_FRONTEND_TYPES', default='false') == 'true':
         path = os.getenv('FOLDER_TO_SAVE_FRONTEND_TYPES', default='frontend')
         generate_typescript_defs(
