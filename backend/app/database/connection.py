76ef698c571fbc7b748a0300c3856f7482fcd96c
vagechirkov <vagechirkov@gmail.com>
2022-08-15 21:13:21 +0200

    separate database connection functions

---
+++ backend/app/database/connection.py
@@ -0,0 +1,63 @@
+import os
+from typing import Optional, List, Any
+
+from beanie import init_beanie, PydanticObjectId
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseSettings, BaseModel
+
+from models.session import Session
+
+
+class Settings(BaseSettings):
+    # reading variables from the environment
+    MONGO_URL: Optional[str] = 'mongodb://localhost:3002/'
+    APP_NAME = 'reward-network-ii'
+
+    async def initialize_database(self):
+        client = AsyncIOMotorClient(self.MONGO_URL)
+        await init_beanie(
+            database=client[self.APP_NAME],
+            document_models=[
+                Session
+            ])
+
+
+class Database:
+    def __init__(self, model):
+        self.model = model
+
+    async def save(self, document):
+        await document.create()
+        return
+
+    async def get(self, id: PydanticObjectId) -> bool:
+        doc = await self.model.get(id)
+        if doc:
+            return doc
+        return False
+
+    async def get_all(self) -> List[Any]:
+        docs = await self.model.find_all().to_list()
+        return docs
+
+    async def update(self, id: PydanticObjectId, body: BaseModel) -> Any:
+        doc_id = id
+        des_body = body.dict()
+
+        des_body = {k: v for k, v in des_body.items() if v is not None}
+        update_query = {"$set": {
+            field: value for field, value in des_body.items()
+        }}
+
+        doc = await self.get(doc_id)
+        if not doc:
+            return False
+        await doc.update(update_query)
+        return doc
+
+    async def delete(self, id: PydanticObjectId) -> bool:
+        doc = await self.get(id)
+        if not doc:
+            return False
+        await doc.delete()
+        return True
