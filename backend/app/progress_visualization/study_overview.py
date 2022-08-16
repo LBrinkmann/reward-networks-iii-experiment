45c5ad807d110ee92a3e5308694e622c5f5d5d34
vagechirkov <vagechirkov@gmail.com>
2022-08-16 13:40:28 +0200

    add modifications to the graph

--- backend/app/progress_visualization/study_overview.py
+++ backend/app/progress_visualization/study_overview.py
@@ -4,9 +4,8 @@ from pyvis.network import Network
 
 
 def create_sessions_network():
-    net = Network(
-        height='750px', width='100%', bgcolor='#222222', font_color='white',
-        directed=True, layout=True)
+    # , bgcolor='#222222', font_color='white',
+    net = Network(height='800px', width='100%', directed=True, layout=True)
     # net.barnes_hut()
     for g in range(4):
         # two parallel sessions in neighboring generations
@@ -17,30 +16,11 @@ def create_sessions_network():
             inx1 = f'g_{g + 1}_s_{g_1}'
             net.add_node(inx0, inx0, level=g, x=g_0 * 5)
             net.add_node(inx1, inx1, level=g + 1, x=g_1 * 5)
-            net.add_edge(inx0, inx1, value=0.1)
+            net.add_edge(inx0, inx1)
 
     # net.show_buttons(filter_=['edges'])
     # net.show_buttons()
-    net.set_options(
-        """
-            {
-              "layout": {
-                "hierarchical": {
-                  "enabled": true,
-                  "levelSeparation": 245
-                }
-              },
-              "interaction": {
-                "hover": true,
-                "navigationButtons": true
-              },
-              "edges": {
-                "hoverWidth": 2,
-                "selectionWidth": 4
-              }
-            }
-        """
-    )
+    net.set_options(open('graph_settings.json').read())
     net.show('study_net.html')
 
 
