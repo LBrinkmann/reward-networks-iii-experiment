508fba6ef74d4836c92845879d09654364f11a80
vagechirkov <vagechirkov@gmail.com>
2022-08-19 00:27:56 +0200

    minor changes

--- frontend/src/Study/Study.tsx
+++ frontend/src/Study/Study.tsx
@@ -2,22 +2,27 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 const Study = () => {
-     const [trial, setTrial] = useState(null as string);
-     const [block, setBlock] = useState(null as string);
+    const [trialType, setTrialType] = useState(null as string);
 
-     switch (block)
-     {
-         case 'intro':
-             return <> </>; // <Intro />
-         case 'tutorial':
-             return <> </>; // <Tutorial />
-         case 'learning':
-             return <> </>; // <Learning />
-         case  'advise':
-             return <> </>; // <Advise>
-         default:
-             return <> </>;
-     }
+    // fetch data for the given trial
+    // TODO: implement trial fetching
+
+    // send results at the end of the trial
+
+    // use trial info as props for a corresponding component
+
+    switch (trialType) {
+        case 'intro':
+            return <> </>; // <Intro />
+        case 'tutorial':
+            return <> </>; // <Tutorial />
+        case 'learning':
+            return <> </>; // <Learning />
+        case  'advise':
+            return <> </>; // <Advise />
+        default:
+            return <> </>;
+    }
 };
 
 
