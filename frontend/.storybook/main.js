735dcadebeef36e886d1ba8b87cfd9766128f12d
vagechirkov <vagechirkov@gmail.com>
2022-08-20 11:39:54 +0200

    update storybook setup and docker

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,26 +1,15 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: {
-      name: "@storybook/builder-webpack5",
-      options: {
-        webpackDevMiddleware: (config) => {
-          config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 300,
-          };
-          return config;
-        },
-      },
-    },
-  },
-};
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
