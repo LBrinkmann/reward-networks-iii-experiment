10ea7787c76b3932b59afae9883eb45f8e1bb225
vagechirkov <vagechirkov@gmail.com>
2022-08-28 15:23:29 +0200

    refactor static network step 1

--- frontend/src/components/Network/StaticNetwork/StaticNetwork.stories.tsx
+++ frontend/src/components/Network/StaticNetwork/StaticNetwork.stories.tsx
@@ -14,30 +14,20 @@ const Template: ComponentStory<typeof StaticNetwork> = (args) => <StaticNetwork 
 export const FirstStory = Template.bind({});
 
 FirstStory.args = {
-    actions: [
-        {
-            "annotation": "-20",
-            "source": {"nodeIdx": 0, "displayName": "A", "x": 0.7, "y": 0.2},
-            "target": {"nodeIdx": 1, "displayName": "B", "x": 0.3, "y": 0.2},
-            "colorClass": "large-negative",
-            "linkStyle": "dashed"
-        },
-        {
-            "annotation": "120",
-            "source": {"nodeIdx": 5, "displayName": "F", "x": 0.9, "y": 0.5},
-            "target": {"nodeIdx": 2, "displayName": "C", "x": 0.1, "y": 0.5},
-            "colorClass": "positive",
-            "linkStyle": "animated"
-        },
-
+    edges: [
+        {source_num: 0, target_num: 1, reward: -120},
+        {source_num: 2, target_num: 3, reward: 20},
+        {source_num: 3, target_num: 5, reward: -20},
+        {source_num: 4, target_num: 3, reward: 120},
+        {source_num: 5, target_num: 0, reward: 120},
     ],
     nodes: [
-        {"nodeIdx": 0, "displayName": "A", "x": 0.7, "y": 0.2, status: 'active'},
-        {"nodeIdx": 1, "displayName": "B", "x": 0.3, "y": 0.2, status: 'disabled'},
-        {"nodeIdx": 5, "displayName": "F", "x": 0.9, "y": 0.5},
-        {"nodeIdx": 2, "displayName": "C", "x": 0.1, "y": 0.5},
-        {"nodeIdx": 4, "displayName": "E", "x": 0.7, "y": 0.8},
-        {"nodeIdx": 3, "displayName": "D", "x": 0.3, "y": 0.8}
+        {"node_num": 0, "display_name": "A", "x": 0.7, "y": 0.2, status: 'active'},
+        {"node_num": 1, "display_name": "B", "x": 0.3, "y": 0.2, status: 'disabled'},
+        {"node_num": 5, "display_name": "F", "x": 0.9, "y": 0.5, status: 'disabled'},
+        {"node_num": 2, "display_name": "C", "x": 0.1, "y": 0.5, status: 'disabled'},
+        {"node_num": 4, "display_name": "E", "x": 0.7, "y": 0.8, status: 'disabled'},
+        {"node_num": 3, "display_name": "D", "x": 0.3, "y": 0.8, status: 'disabled'},
     ],
 
 
--- frontend/src/components/Network/StaticNetwork/StaticNetwork.tsx
+++ frontend/src/components/Network/StaticNetwork/StaticNetwork.tsx
@@ -1,27 +1,9 @@
 import React from "react";
 
-import LinkMarker from "../LinkMarker";
-import Links from "../Links";
 import NetworkNode from "../NetworkNode";
 import {NetworkNodeInterface} from "../NetworkNode/NetworkNode";
 
-import "../../../main.less"
-import {scaleXY} from "../Links/Links";
-
-type colorClasses = "large-negative" | "negative" | "positive" | "large-positive";
-type LinkStyle = "normal" | "highlighted" | "animated" | "dashed";
-
-export interface Action {
-    actionIdx: number;
-    sourceIdx: number;
-    targetIdx: number;
-    actionTypeIdx: number;
-    source: NetworkNodeInterface;
-    target: NetworkNodeInterface;
-    colorClass: colorClasses;
-    annotation: string;
-    linkStyle: LinkStyle;
-}
+import NetworkEdge from "../NetworkEdge";
 
 export interface Size {
     width: number;
@@ -29,57 +11,78 @@ export interface Size {
 }
 
 interface StaticNetworkInterface {
-    actions: Action[];
+    edges: { reward: number; source_num: number; target_num: number}[];
     nodes: NetworkNodeInterface[];
     onNodeClick?: (nodeIdx: number) => void;
     version?: string;
     size?: Size;
-    nodeSize?: number;
     networkId?: string;
     linkCurvation?: number;
     linkWidth?: number;
 }
 
 const StaticNetwork = ({
-                     actions,
-                     nodes,
-                     onNodeClick = (nodeIdx) => null,
-                     version = "",
-                     size = {width: 550, height: 550},
-                     nodeSize = 600 / 15,
-                     networkId = "default",
-                     linkCurvation,
-                     linkWidth = 5,
-                 }: StaticNetworkInterface) => {
-    const effNodeSize = nodeSize ? nodeSize : ((size.height / 550) * 600) / 15;
+                           edges,
+                           nodes,
+                           onNodeClick = (nodeIdx) => null,
+                           version = "",
+                           size = {width: 550, height: 550},
+                           networkId = "default",
+                           linkCurvation,
+                           linkWidth = 5,
+                       }: StaticNetworkInterface) => {
+    /* Scale node coordinates */
+    const scaleXY = (
+        node: { x: number; y: number },
+        size: { width: number; height: number }
+    ) => ({
+        x: node.x * size.width,
+        y: node.y * size.height,
+    });
+
+    const scaledNodes = nodes.map((node) => ({
+        ...node,
+        ...scaleXY(node, size),  // scaled coordinates
+        // ... () => {node.node_size ? node.node_size : ((size.height / 550) * 600) / 15},
+    } as NetworkNodeInterface));
+
     return (
         <svg
-            className={`network-game ${version}`}
+            // className={`network-game ${version}`}
             width={size.width}
             height={size.height}
         >
-            <LinkMarker
-                networkId={networkId}
-                nodeSize={effNodeSize}
-                linkWidth={linkWidth}
-                linkCurvation={linkCurvation}
-            />
-            <Links
-                actions={actions}
-                size={size}
-                networkId={networkId}
-                linkWidth={linkWidth}
-                linkCurvation={linkCurvation}
-            />
+            {/*<LinkMarker*/}
+            {/*    networkId={networkId}*/}
+            {/*    nodeSize={effNodeSize}*/}
+            {/*    linkWidth={linkWidth}*/}
+            {/*    linkCurvation={linkCurvation}*/}
+            {/*/>*/}
             <g>
-                {nodes.map((node, idx) => {
+                {edges.map((edge, idx) => {
+                    return (
+                        <NetworkEdge
+                            reward={edge.reward}
+                            source={scaledNodes[edge.source_num]}
+                            target={scaledNodes[edge.target_num]}
+                            width={linkWidth}
+                            linkCurvation={linkCurvation}
+                            key={"link-" + idx}
+                            networkId={networkId}
+                            idx={idx}
+                            // linkStyle={edge.linkStyle}
+                        />
+                    );
+                })}
+            </g>
+            <g>
+                {scaledNodes.map((node, idx) => {
                     return (
                         <NetworkNode
-                            {...scaleXY(node, size)}
-                            nodeSize={effNodeSize}
+                            {...node}
+                            node_size={((size.height / 550) * 600) / 15}
                             onNodeClick={onNodeClick}
                             key={"point-" + idx}
-                            networkId={networkId}
                         />
                     );
                 })}
