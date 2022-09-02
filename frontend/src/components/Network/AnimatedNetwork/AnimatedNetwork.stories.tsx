836909062d1d9323ac6749b6ebae17b237ec7057
vagechirkov <vagechirkov@gmail.com>
2022-09-02 09:41:13 +0200

    add animated network

---
+++ frontend/src/components/Network/AnimatedNetwork/AnimatedNetwork.stories.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import AnimatedNetwork from './AnimatedNetwork';
+import {Paper} from "@mui/material";
+
+export default {
+    title: 'Network/AnimatedNetwork',
+    component: AnimatedNetwork,
+} as ComponentMeta<typeof AnimatedNetwork>;
+
+const Template: ComponentStory<typeof AnimatedNetwork> = (args) => {
+    return (
+        <Paper
+            sx={{
+                p: 2,
+                margin: 'auto',
+                maxWidth: 550,
+                flexGrow: 1
+            }}
+        >
+            <AnimatedNetwork {...args}/>
+        </Paper>
+    )
+};
+
+export const TenNodes = Template.bind({});
+
+TenNodes.args = {
+    edges: [
+        {source_num: 0, target_num: 1, reward: -120},
+        {source_num: 2, target_num: 3, reward: 20},
+        {source_num: 3, target_num: 5, reward: -20},
+        {source_num: 4, target_num: 3, reward: 120},
+        {source_num: 5, target_num: 0, reward: 120},
+        {source_num: 2, target_num: 8, reward: -20},
+        {source_num: 4, target_num: 6, reward: 120},
+        {source_num: 4, target_num: 2, reward: 120},
+    ],
+    nodes: [
+        {
+            "node_num": 0,
+            "display_name": "A",
+            "x": (Math.cos(36 * 1 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 1 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 1,
+            "display_name": "B",
+            "x": (Math.cos(36 * 2 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 2 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 2,
+            "display_name": "C",
+            "x": (Math.cos(36 * 3 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 3 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 3,
+            "display_name": "D",
+            "x": (Math.cos(36 * 4 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 4 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 4,
+            "display_name": "E",
+            "x": (Math.cos(36 * 5 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 5 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "is_starting": true,
+        },
+        {
+            "node_num": 5,
+            "display_name": "F",
+            "x": (Math.cos(36 * 6 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 6 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 6,
+            "display_name": "G",
+            "x": (Math.cos(36 * 7 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 7 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 7,
+            "display_name": "H",
+            "x": (Math.cos(36 * 8 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 8 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 8,
+            "display_name": "I",
+            "x": (Math.cos(36 * 9 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 9 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+        {
+            "node_num": 9,
+            "display_name": "J",
+            "x": (Math.cos(36 * 0 * (Math.PI / 180)) + 1) / 3 + 0.16,
+            "y": (Math.sin(36 * 0 * (Math.PI / 180)) + 1) / 3 + 0.16,
+        },
+
+    ],
+    moves: [4, 2, 3, 5, 0, 1]
+};
---
+++ frontend/src/components/Network/AnimatedNetwork/AnimatedNetwork.tsx
@@ -0,0 +1,92 @@
+import React, {useEffect, useState} from "react";
+import StaticNetwork from "../StaticNetwork";
+import {StaticNetworkEdgeInterface, StaticNetworkNodeInterface} from "../StaticNetwork/StaticNetwork";
+import {Button, Grid} from "@mui/material";
+import {NetworkEdgeStyle} from "../NetworkEdge/NetworkEdge";
+
+export interface AnimatedNetworkInterface {
+    nodes: StaticNetworkNodeInterface[];
+    edges: StaticNetworkEdgeInterface[];
+    moves: number[];
+    delayBetweenMoves: number;
+}
+
+const AnimatedNetwork: React.FC<AnimatedNetworkInterface> = (
+    {
+        delayBetweenMoves = 1000,
+        ...props
+    }: AnimatedNetworkInterface) => {
+    const [currentNodeId, setCurrentNodeId] = useState<number>(props.moves[0]);
+    const [currentMoveInx, setCurrentMoveInx] = useState<number>(null);
+    const [nodes, setNodes] = useState<StaticNetworkNodeInterface[]>(props.nodes);
+    const [edges, setEdges] = useState<StaticNetworkEdgeInterface[]>(props.edges);
+
+    useEffect(() => {
+        if (currentMoveInx === null) {
+            return;
+        }
+        setTimeout(() => {
+            setCurrentNodeId(props.moves[currentMoveInx]);
+            // setNodes(updateNodes(props.moves[currentMoveInx]));
+            if (currentMoveInx !== 0) {
+                setEdges(updateEdges(props.moves[currentMoveInx - 1], props.moves[currentMoveInx]));
+            }
+            setCurrentMoveInx(currentMoveInx + 1);
+        }, delayBetweenMoves);
+    }, [currentMoveInx]);
+
+    // const updateNodes = (nodeInx: number) => {
+    //     return nodes.map((node) => {
+    //         if (node.node_num === nodeInx) {
+    //             return {...node, isActive: true};
+    //         }
+    //         return node;
+    //     });
+    // }
+
+    const updateEdges = (sourceInx: number, targetInx: number) => {
+        return edges.map((edge: StaticNetworkEdgeInterface) => {
+            if (edge.source_num === sourceInx && edge.target_num === targetInx) {
+                return {...edge, edgeStyle: "highlighted" as NetworkEdgeStyle};
+            } else {
+                return {...edge, edgeStyle: "normal" as NetworkEdgeStyle};
+            }
+        });
+    }
+
+
+    const startReplayHandler = () => {
+        if (currentMoveInx === null) {
+            setCurrentMoveInx(0);
+            console.log("start replay");
+        }
+    }
+
+
+    return (
+        <Grid sx={{flexGrow: 1}} direction="column" container spacing={4} justifyContent="center" alignItems="center">
+            <Grid item>
+                <StaticNetwork
+                    edges={edges}
+                    nodes={nodes}
+                    currentNodeId={currentNodeId}
+                    possibleMoves={[]}
+                    size={{width: 550, height: 550}}
+                    onNodeClickHandler={() => {
+                    }}
+                />
+            </Grid>
+            <Grid item>
+                {currentMoveInx !== null ? (
+                    <Button variant="contained" color="primary" disabled>Start Replay</Button>
+                ) : (
+                    <Button onClick={startReplayHandler} variant="contained" color="primary">Start Replay</Button>
+                )}
+            </Grid>
+
+        </Grid>
+    )
+}
+
+
+export default AnimatedNetwork;
