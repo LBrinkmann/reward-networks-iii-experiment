2072aba6912adcb9f1553fecfb8a9e45ab741912
vagechirkov <vagechirkov@gmail.com>
2022-09-19 09:09:29 +0200

    Add written strategy component

---
+++ frontend/src/components/Trials/SocialLearning/WrittenStrategy/WrittenStrategy.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import WrittenStrategy from "./WrittenStrategy";
+import Header from "../../../Header";
+
+export default {
+    title: 'Trials/SocialLearning/WrittenStrategy',
+    component: WrittenStrategy,
+} as ComponentMeta<typeof WrittenStrategy>;
+
+const Template: ComponentStory<typeof WrittenStrategy> = function (args) {
+    return (
+        <>
+            <Header totalPoints={0} title={"Written Strategy"}/>
+            <WrittenStrategy/>
+        </>
+    );
+};
+
+export const DefaultStory = Template.bind({});
+
+DefaultStory.args = {
+};
