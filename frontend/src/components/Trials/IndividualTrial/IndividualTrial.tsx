8117145e55a22a1f37e152643845a84b11b0db75
vagechirkov <vagechirkov@gmail.com>
2022-09-19 15:27:27 +0200

    polish individual trial component

--- frontend/src/components/Trials/IndividualTrial/IndividualTrial.tsx
+++ frontend/src/components/Trials/IndividualTrial/IndividualTrial.tsx
@@ -1,16 +1,20 @@
 import React, {useState} from "react";
-import {Grid, Paper} from "@mui/material";
+import {Box, Grid, Paper, Typography} from "@mui/material";
 import DynamicNetwork from "../../Network/DynamicNetwork";
 import {DynamicNetworkInterface} from "../../Network/DynamicNetwork/DynamicNetwork";
+import Timer from "./Timer";
 
 
 export interface IndividualTrialInterface extends DynamicNetworkInterface {
-
+    /** Timer duration in seconds */
+    timer?: number
 }
 
-const IndividualTrial: React.FC<IndividualTrialInterface> = (props) => {
+const IndividualTrial: React.FC<IndividualTrialInterface> = ({timer = 2 * 60, ...props}) => {
     const [step, setStep] = useState<number>(0);
     const [points, setPoints] = useState<number>(0);
+    const [isDone, setIsDone] = useState<boolean>(false);
+
 
     const onNodeClickHandler = (currentNode: number, nextNode: number) => {
         // Update state
@@ -23,21 +27,39 @@ const IndividualTrial: React.FC<IndividualTrialInterface> = (props) => {
     }
 
     return (
-        <Paper sx={{p: 2, margin: 'auto', maxWidth: 550, flexGrow: 1}}>
-            <Grid sx={{flexGrow: 1}} direction="column" container spacing={8} justifyContent="center"
-                  alignItems="center">
-                <Grid item alignItems="center" style={{textAlign: "center"}}>
+        <Paper sx={{p: 2, margin: 'auto', maxWidth: 700, flexGrow: 1}}>
+            <Grid sx={{flexGrow: 1}} direction="row" container spacing={2}>
+                {/* Network */}
+                <Grid item>
                     <DynamicNetwork
                         nodes={props.nodes}
                         edges={props.edges}
                         onNodeClickParentHandler={onNodeClickHandler}
+                        isDisabled={isDone}
                     />
                 </Grid>
-                <Grid item>
-                    Step={step}
-                    Cumulative Points={points}
-                </Grid>
+                <Grid item sm container>
+                    <Grid sx={{flexGrow: 1}} direction="column" container spacing={2}>
+                        {/* Timer */}
+                        <Box sx={{margin: "10px"}} justifyContent="center">
+                            <Timer time={timer} OnTimeEndHandler={() => setIsDone(true)}/>
+                        </Box>
+                        {/* Information */}
+                        <Box sx={{p: 2, margin: "10px"}} justifyContent="center">
+                            <Grid item>
+                                <Typography variant="h5" component="div">
+                                    Step {step}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h5" component="div">
+                                    Points {points}
+                                </Typography>
+                            </Grid>
+                        </Box>
+                    </Grid>
 
+                </Grid>
             </Grid>
         </Paper>
     );
