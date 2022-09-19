31033ff88cd71c66c3788bae6be3e69c2d8e68fd
vagechirkov <vagechirkov@gmail.com>
2022-09-20 00:32:11 +0200

    update examples

--- frontend/src/components/Network/examples/index.tsx
+++ frontend/src/components/Network/examples/index.tsx
@@ -1,9 +1,7 @@
-const data = require('../../Network/examples/train.json');
+const data = require('../../Network/examples/examples.json');
 
 data.forEach((d: any) => {
     d.nodes.forEach((n: any, inx: number) => {
-        n.x = (Math.cos(36 * n.node_num * (Math.PI / 180)) + 1) / 3 + 0.1;
-        n.y = (Math.sin(36 * n.node_num * (Math.PI / 180)) + 1) / 3 + 0.1;
         if (inx === 0) {
             n.is_starting = true;
         }
