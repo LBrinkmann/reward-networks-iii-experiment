f0c9fdf7bc3911f8f7f913903eaa1a74ce63e559
vagechirkov <vagechirkov@gmail.com>
2022-09-26 13:03:39 +0200

    copy Network model from https://github.com/LBrinkmann/reward-network-iii-algorithm/blob/sara_branch/models/network.py

--- backend/app/models/trial.py
+++ backend/app/models/trial.py
@@ -1,42 +1,9 @@
 import datetime
 from typing import Optional, List, Literal
 
+from models.network import Network
 from beanie import PydanticObjectId
 from pydantic import BaseModel
-
-
-class Node(BaseModel):
-    node_idx: int
-    display_name: str
-    x: float
-    y: float
-    source_edge_idx: List[int]
-
-
-class EdgeType(BaseModel):
-    action_type_idx: int
-    reward: int
-
-
-class Edge(BaseModel):
-    source_idx: int
-    target_idx: int
-    edge_idx: int
-    edge_type_idx: int
-
-    @classmethod
-    def parse(cls, idx, **action):
-        return cls(
-            source_idx=action['sourceId'],
-            target_idx=action['targetId'],
-            edge_idx=idx,
-            edge_type_idx=action['rewardId'])
-
-
-class Network(BaseModel):
-    nodes: List[Node]
-    edges: List[Edge]
-    edge_types: List[EdgeType]
 
 
 class Solution(BaseModel):
