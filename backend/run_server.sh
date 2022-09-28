7414e9b6401498b5af67eb18bf886f08098b2c19
vagechirkov <vagechirkov@gmail.com>
2022-09-28 12:31:52 +0200

    fix fastAPI server deployment

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -2,18 +2,9 @@
 set -e -x
 
 # Runs as the "server" user in the Docker container.
-
 export HOME=/home/server
 export PATH="$VIRTUAL_ENV/bin:$PATH"
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-echo $(pwd)
-
-ls
-# For development
-# env FLASK_APP=server.py FLASK_ENV=development flask run --host=0.0.0.0
-
-# For production
-
-uvicorn app.server:app --port 5000 --host 0.0.0.0
+uvicorn app.server:api --port 5000 --host 0.0.0.0
