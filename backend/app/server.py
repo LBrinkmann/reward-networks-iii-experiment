1f5baa52df3ed8c5cf240ab318a7fe41aab2d32d
vagechirkov <vagechirkov@gmail.com>
2022-09-28 12:54:57 +0200

    generate session on server start

--- backend/app/server.py
+++ backend/app/server.py
@@ -4,10 +4,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from database.connection import Settings
+from models.session import Session
+from models.subject import Subject
 # from routes.advise import advise_router
 from routes.progress import progress_router
 from routes.session import session_router
 from routes.simulate_study import simulation_router
+from study_setup.generate_sessions import generate_sessions
 
 if os.getenv('GENERATE_FRONTEND_TYPES', default='false') == 'true':
     from pydantic2ts import generate_typescript_defs
@@ -43,3 +46,12 @@ async def startup_event():
         path = os.getenv('FOLDER_TO_SAVE_FRONTEND_TYPES', default='frontend')
         generate_typescript_defs(
             'app.models', os.path.join(path, 'apiTypes.ts'))
+
+    # run the study simulation
+    await Session.find().delete()
+    await Subject.find().delete()
+    await generate_sessions(n_generations=5,
+                            n_sessions_per_generation=10,
+                            n_advise_per_session=5,
+                            experiment_type='reward_network_iii',
+                            experiment_num=0)
