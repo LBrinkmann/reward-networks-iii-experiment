bf765b40f1f0e28a18f4d9fe0cc24572e9fcd9b5
vagechirkov <vagechirkov@gmail.com>
2022-10-03 12:07:03 +0200

    rename nodes in storybook

--- frontend/src/components/Network/examples/index.tsx
+++ frontend/src/components/Network/examples/index.tsx
@@ -1,7 +1,12 @@
 const data = require('../../Network/examples/train_viz.json');
 
-data.forEach((n: any) => {
-    n.nodes[n.starting_node].starting_node = true;
+const nodeLabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
+data.forEach((net: any) => {
+    net.nodes[net.starting_node].starting_node = true;
+    net.nodes.forEach((node: any, inx: number) => {
+        node.display_name = nodeLabels[inx];
+    });
 });
 
 export default data;
