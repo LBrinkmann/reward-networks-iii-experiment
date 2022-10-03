ec253fcc6f43d38a1240e89e634c64b77105b8fb
vagechirkov <vagechirkov@gmail.com>
2022-10-03 16:28:22 +0200

    add player information as a separate component

---
+++ frontend/src/components/Trials/SocialLearning/PlayerInformation/PlayerInformation.tsx
@@ -0,0 +1,44 @@
+import {Paper, Typography} from "@mui/material";
+import React, {FC} from "react";
+
+interface PlayerInformationProps {
+    /** Player's ID */
+    id: number;
+    step: number;
+    cumulativePoints: number;
+    totalPoints: number;
+    /** Player's comment */
+    comment?: string;
+}
+
+
+export const PlayerInformation:FC<PlayerInformationProps> = (props) => {
+    return (
+        <>
+            <Paper sx={{p: 2}} variant="outlined">
+                <Typography gutterBottom variant="h4" component="div" align={'center'}>
+                    Player {props.id}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    Step {props.step}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    Cumulative points {props.cumulativePoints}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    Total points {props.totalPoints}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    Comment:
+                </Typography>
+                <Paper sx={{p: 2, maxHeight: "200px", overflow: 'auto'}}>
+                    <Typography variant="body1">
+                        {props.comment ? props.comment : "No comment"}
+                    </Typography>
+                </Paper>
+            </Paper>
+        </>
+    )
+}
+
+export default PlayerInformation;
