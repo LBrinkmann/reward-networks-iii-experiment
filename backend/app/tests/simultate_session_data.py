232d8772e1b177fdb248742598fb4bc333b4dd49
vagechirkov <vagechirkov@gmail.com>
2022-10-15 11:41:00 +0200

    refactor data simulation

--- backend/app/tests/simultate_session_data.py
+++ backend/app/tests/simultate_session_data.py
@@ -2,42 +2,17 @@ import json
 import random
 from pathlib import Path
 
-from fastapi import APIRouter
-
+from models.config import ExperimentSettings
 from models.network import Network
 from models.session import Session
-from models.subject import Subject
 from models.trial import Trial, Solution, WrittenStrategy
 from utils.utils import estimate_solution_score
-from study_setup.generate_sessions import generate_sessions
 
-simulation_router = APIRouter(tags=["Simulation"])
 network_data = json.load(open(Path('data') / 'train_viz.json'))
-
 solutions = json.load(
     open(Path('data') / 'solution_moves_take_first_loss_viz.json'))
 
-n_demonstration_trials = 3
-
-
-@simulation_router.get('/{experiment_num}')
-async def get_study_simulation(experiment_num: int = 0,
-                               new_sessions: bool = False,
-                               generation: int = 0):
-    """ http://localhost:5000/simulation/0?new_sessions=true&generation=0 """
-    random.seed(42)
-    if new_sessions:
-        await Session.find().delete()
-        await Subject.find().delete()
-        await generate_sessions(n_generations=5,
-                                n_sessions_per_generation=20,
-                                n_advise_per_session=5,
-                                experiment_type='reward_network_iii',
-                                experiment_num=experiment_num)
-    if generation != 0:
-        await simulate_data(generation)
-
-    return {'done': True}
+config = ExperimentSettings()
 
 
 async def simulate_data(generation):
@@ -50,7 +25,7 @@ async def simulate_data(generation):
             trials = []
 
             # Demonstration trial
-            for i in range(n_demonstration_trials):
+            for i in range(config.n_demonstration_trials):
                 moves = [0, 5, 3, 4, 0, 5, 6, 7, 9]
                 network = Network.parse_obj(
                     network_data[random.randint(0, network_data.__len__() - 1)])
@@ -74,7 +49,7 @@ async def simulate_data(generation):
 
             # Written strategy
             trials.append(Trial(
-                id=n_demonstration_trials + 1,
+                id=config.n_demonstration_trials + 1,
                 trial_type='written_strategy',
                 written_strategy=WrittenStrategy(strategy=''))
             )
