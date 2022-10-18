2f1386e30730a358d988bef770861e03de2a0030
vagechirkov <vagechirkov@gmail.com>
2022-10-18 09:27:10 +0200

    refactor social learning selection component

--- frontend/src/components/Trials/SocialLearning/Selection/Selection.tsx
+++ frontend/src/components/Trials/SocialLearning/Selection/Selection.tsx
@@ -9,20 +9,24 @@ interface SocialLearningSelectionProps {
 }
 
 const Selection: React.FC<SocialLearningSelectionProps> = (props: SocialLearningSelectionProps) => {
+    const renderOneCard = (advisor: { advisorId: string, averageScore: number }, inx: number) => {
+        const onClickHandler = () => {
+            props.onClickHandler(advisor.advisorId, inx);
+        }
+        return (
+            <Grid item key={inx}>
+                <SelectionOneCard
+                    personInx={inx}
+                    averageScore={advisor.averageScore}
+                    onClickHandler={onClickHandler}/>
+            </Grid>
+        )
+    }
+
     return (
         <Grid sx={{flexGrow: 1}} container spacing={8} justifyContent="center">
             {props.advisors.map((advisor, inx) => {
-                const onClickHandler = () => {
-                    props.onClickHandler(advisor.advisorId, inx);
-                }
-                return (
-                    <Grid item key={inx}>
-                        <SelectionOneCard
-                            personInx={inx}
-                            averageScore={advisor.averageScore}
-                            onClickHandler={onClickHandler}/>
-                    </Grid>
-                )
+                return renderOneCard(advisor, inx);
             })}
         </ Grid>
     );
