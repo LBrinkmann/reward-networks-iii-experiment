7aa5e80f66769f6e5ef04f1a2d00af79525374da
vagechirkov <vagechirkov@gmail.com>
2022-10-21 15:00:18 +0200

    refactor tutorial tooltips content location

---
+++ frontend/src/components/Tutorial/TutorialTip/TutorialTooltipContent.ts
@@ -0,0 +1,27 @@
+// NOTE: id, title, text and tip should be non-empty strings
+export const tutorialTooltipContent = [
+    {
+        id: "practice_node",
+        title: "Network Nodes",
+        text: "You start at the highlighted circle in the network.",
+        tip: "Click a node",
+    },
+    {
+        id: "practice_step_score",
+        title: "Score & Step",
+        text: "You always have 8 moves per network. Your goal is to collect the maximum total number of points in these 8 moves.",
+        tip: "Current step and cumulative score",
+    },
+    {
+        id: "practice_linear_solution",
+        title: "Your Solution",
+        text: "As you proceed in the network, your progress is noted here. Now finish the trial by making your 8 moves.",
+        tip: "Your Solution",
+    },
+    {
+        id: "social_learning_selection_player",
+        title: "Player selection",
+        text: `Select a player to see the solution of this player`,
+        tip: "Select a player",
+    },
+];
