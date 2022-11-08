9303a46e4bd12304cc6ac1fd3593cc2ad431b72a
vagechirkov <vagechirkov@gmail.com>
2022-11-08 22:44:02 +0100

    add legend and update player info

--- frontend/src/components/Trials/SocialLearning/PlayerInformation/PlayerInformation.tsx
+++ frontend/src/components/Trials/SocialLearning/PlayerInformation/PlayerInformation.tsx
@@ -1,6 +1,9 @@
-import {Paper, TextField, Typography} from "@mui/material";
+import {CardMedia, Divider, Paper, Stack, TextField, Typography} from "@mui/material";
 import React, {FC} from "react";
 import TutorialTip from "../../../Tutorial/TutorialTip";
+// @ts-ignore
+import rewardsImg from "./legend.png";
+import styled from "@emotion/styled";
 
 interface PlayerInformationProps {
     /** Player's ID */
@@ -17,15 +20,40 @@ interface PlayerInformationProps {
     onTutorialClose?: () => void;
 }
 
+const Item = styled(Paper)(() => ({
+    padding: 2,
+    elevation: 0,
+    textAlign: 'left',
+}));
+
+const PlayerInfoItem:FC = ({children}) => {
+    return (
+        <Item elevation={0}>
+            {children}
+            <Divider/>
+        </Item>
+    )
+};
+
 
 export const PlayerInformation: FC<PlayerInformationProps> = (props) => {
     const {showComment = true, showTutorialScore = false, showTutorialComment = false} = props;
     return (
-        <>
-            <Paper sx={{p: 2}} elevation={0}>
+
+        <Stack spacing={1} sx={{paddingTop: "20px"}}>
+            <PlayerInfoItem>
+                <CardMedia
+                    component="img"
+                    image={rewardsImg}
+                    alt="You earn or lose points depending on the color of the arrow."
+                />
+            </PlayerInfoItem>
+            <PlayerInfoItem>
                 <Typography gutterBottom variant="h5" component="div">
                     Step {props.step}
                 </Typography>
+            </PlayerInfoItem>
+            <PlayerInfoItem>
                 <TutorialTip
                     tutorialId={"practice_step_score"}
                     isTutorial={showTutorialScore}
@@ -36,32 +64,34 @@ export const PlayerInformation: FC<PlayerInformationProps> = (props) => {
                         Points {props.cumulativePoints}
                     </Typography>
                 </TutorialTip>
-                {(showComment) ? (
+            </PlayerInfoItem>
+            {(showComment) ? (
+                <PlayerInfoItem>
                     <Typography gutterBottom variant="h5" component="div">
                         Player {props.id} comment:
                     </Typography>
-                ) : null}
-            </Paper>
-            {(showComment) ? (
-                <TutorialTip
-                    tutorialId={"social_learning_observation_comment"}
-                    isTutorial={showTutorialComment}
-                    isShowTip={false}
-                    onTutorialClose={props.onTutorialClose}
-                    placement={"right"}
-                >
-                    <TextField
-                        id="outlined-multiline-static"
-                        // label=""
-                        multiline
-                        fullWidth
-                        rows={10}
-                        InputProps={{readOnly: true}}
-                        defaultValue={props.comment ? props.comment : "No comment"}
-                    />
-                </TutorialTip>
+
+                    <TutorialTip
+                        tutorialId={"social_learning_observation_comment"}
+                        isTutorial={showTutorialComment}
+                        isShowTip={false}
+                        onTutorialClose={props.onTutorialClose}
+                        placement={"right"}
+                    >
+                        <TextField
+                            id="outlined-multiline-static"
+                            // label=""
+                            multiline
+                            fullWidth
+                            rows={10}
+                            InputProps={{readOnly: true}}
+                            defaultValue={props.comment ? props.comment : "No comment"}
+                        />
+                    </TutorialTip>
+                </PlayerInfoItem>
             ) : null}
-        </>
+        </Stack>
+
     )
 }
 
---
+++ frontend/src/components/Trials/SocialLearning/PlayerInformation/TrialWithNetworkLayout.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import PlayerInformation from "./PlayerInformation";
+import {Grid} from "@mui/material";
+
+
+export default {
+    title: 'Trials/PlayerInformation',
+    component: PlayerInformation,
+} as ComponentMeta<typeof PlayerInformation>;
+
+const Template: ComponentStory<typeof PlayerInformation> = function (args) {
+
+    return (
+        <Grid container sx={{margin: 'auto', width: '85%'}} justifyContent="space-around">
+            <Grid item sx={{p: 1}} xs={3}>
+                <Grid container direction="column">
+                    <Grid item xs={4}>
+                        <></>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <PlayerInformation {...args}/>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={7}>
+                <></>
+            </Grid>
+        </Grid>
+    )
+};
+
+export const ExampleOne = Template.bind({});
+
+ExampleOne.args = {
+    id: 1,
+    step: 2,
+    cumulativePoints: 100,
+    comment: "This is a comment",
+    showComment: true,
+
+
+};
+
