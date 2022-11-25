1f0b61f6071cc9c658d5cc2de84ef108f15e8119
vagechirkov <vagechirkov@gmail.com>
2022-11-25 17:27:11 +0100

    add upload button

--- frontend/src/components/Trials/TaskExplorer/TaskExplorer.stories.tsx
+++ frontend/src/components/Trials/TaskExplorer/TaskExplorer.stories.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 
 import TaskExplorer from "./TaskExplorer";
 
-import data from "../../Network/examples";
+import default_data from "../../Network/examples";
+import {Button, Grid, MenuItem, TextField} from "@mui/material";
 
 export default {
     title: 'Utils/TaskExplorer',
@@ -12,9 +13,68 @@ export default {
 } as ComponentMeta<typeof TaskExplorer>;
 
 const Template: ComponentStory<typeof TaskExplorer> = function (args) {
+    const [data, setData] = React.useState(default_data);
+    const [currentExample, setCurrentExample] = useState<number>(1);
+
+    const getData = (e: ChangeEvent<HTMLInputElement>) => {
+        const reader = new FileReader();
+        reader.readAsText(e.target.files[0], "UTF-8");
+        reader.onload = (e) => {
+            // parse file
+            let new_data = JSON.parse(e.target.result.toString());
+            // update data
+            new_data.forEach((net: any) => {
+                net.nodes[net.starting_node].starting_node = true;
+            });
+            if (new_data.length > 0) {
+                setCurrentExample(0);
+                setData(new_data);
+            }
+            // console.log(new_data);
+        }
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentExample(parseInt(event.target.value));
+    };
+
     return (
         <>
-            <TaskExplorer {...args}/>
+            <TaskExplorer {...args}
+                          nodes={data[currentExample].nodes}
+                          edges={data[currentExample].edges}
+            />
+
+            <Grid container direction="row" justifyContent="space-around" spacing={3}>
+                <Grid item>
+                    <input
+                        style={{display: "none"}}
+                        id="contained-button-file"
+                        type="file"
+                        onChange={getData}
+                    />
+                    <label htmlFor="contained-button-file">
+                        <Button variant="contained" color="primary" component="span">
+                            Upload Networks
+                        </Button>
+                    </label>
+                </Grid>
+                <Grid item>
+                    <TextField
+                        id="outlined-basic"
+                        select label="Select Network to show"
+                        variant="outlined"
+                        onChange={handleChange}
+                        value={currentExample} sx={{width: '40ch'}}
+                    >
+                        {
+                            data.map((net: any, inx: number) => (
+                                <MenuItem key={inx} value={inx}> {`${inx} - ${net.network_id}`} </MenuItem>
+                            ))
+                        }
+                    </TextField>
+                </Grid>
+            </Grid>
         </>
     );
 };
@@ -23,8 +83,10 @@ export const Default = Template.bind({});
 
 Default.args = {
     timer: 25,
-    edges: data[0].edges,
-    nodes: data[0].nodes,
-    onNextTrialHandler: () => {location.reload()},
+    edges: default_data[1].edges,
+    nodes: default_data[1].nodes,
+    onNextTrialHandler: () => {
+        location.reload()
+    },
     hideTrial: false
 };
--- frontend/src/components/Trials/TaskExplorer/TaskExplorer.tsx
+++ frontend/src/components/Trials/TaskExplorer/TaskExplorer.tsx
@@ -1,31 +1,17 @@
 import React from "react";
 import DynamicNetwork from "../../Network/DynamicNetwork";
-import {DynamicNetworkInterface} from "../../Network/DynamicNetwork/DynamicNetwork";
 import Timer from "../../Timer";
 import PlayerInformation from "../SocialLearning/PlayerInformation";
 import TrialWithNetworkLayout from "../TrialWithNetworkLayout";
 import useNetworkStates from "../IndividualTrial/NetworkStates";
 import LinearSolution from "../../Network/LinearSolution";
-import {Box, Button} from "@mui/material";
+import {IndividualTrialInterface} from "../IndividualTrial/IndividualTrial";
 
-export interface IndividualTrialInterface extends DynamicNetworkInterface {
-    /** Handle the end of the trial */
-    onNextTrialHandler: (moves?: number[]) => void;
-    /** Timer duration in seconds; 30 seconds by default */
-    timer?: number;
-    /** The maximum number of steps in the trial. Default is 8 steps*/
-    maxSteps?: number;
-    /** number of seconds to wait before the next trial starts. Default 2 seconds*/
-    waitBeforeNextTrial?: number;
-    /** Hide the trial. Default false */
-    hideTrial?: boolean;
-    /** Callback to update player's total score */
-    updateTotalScore?: (score: number) => void;
-    /** Punishment for the incomplete trials */
-    incompleteTrialPunishment?: number;
+export interface TaskExplorerInterface extends IndividualTrialInterface {
+
 }
 
-const TaskExplorer: React.FC<IndividualTrialInterface> = (props) => {
+const TaskExplorer: React.FC<TaskExplorerInterface> = (props) => {
     const {timer = 25, maxSteps = 8, incompleteTrialPunishment = -100} = props;
     const {
         step,
@@ -36,6 +22,12 @@ const TaskExplorer: React.FC<IndividualTrialInterface> = (props) => {
         onNextStepHandler
     } = useNetworkStates(
         props.onNextTrialHandler, props.edges, props.nodes, maxSteps, props.updateTotalScore, incompleteTrialPunishment)
+
+    const resetData = () => {
+        // hacky way to reset the trial data
+        setIsTimerDone(true);
+        window.localStorage.clear();
+    }
 
     const renderNetwork = () => (
         <DynamicNetwork
@@ -70,29 +62,17 @@ const TaskExplorer: React.FC<IndividualTrialInterface> = (props) => {
         OnTimeEndHandler={() => setIsTimerDone(true)}
     />
 
-    const resetData = () => {
-        // hacky way to reset the trial data
-        setIsTimerDone(true);
-        window.localStorage.clear();
-    }
 
     return (
-        <>
-            <TrialWithNetworkLayout
-                network={renderNetwork()}
-                timer={renderTimer()}
-                playerInformation={renderPlayerInformation()}
-                linearSolution={renderLinearSolution()}
-                showTimer={true}
-                showPlayerInformation={true}
-                showLinearSolution={true}
-            />
-            {/* put the button in the middle of the screen */}
-            <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
-
-                <Button variant="contained" color="error" onClick={resetData}> Reset </Button>
-            </Box>
-        </>
+        <TrialWithNetworkLayout
+            network={renderNetwork()}
+            timer={renderTimer()}
+            playerInformation={renderPlayerInformation()}
+            linearSolution={renderLinearSolution()}
+            showTimer={true}
+            showPlayerInformation={true}
+            showLinearSolution={true}
+        />
     );
 };
 
