b5ac5aca22df29651a0da557cee8a62f3b136c6f
vagechirkov <vagechirkov@gmail.com>
2022-12-12 13:38:16 +0100

    add admin client

--- backend/app/tests/test_progress_route.py
+++ backend/app/tests/test_progress_route.py
@@ -7,10 +7,11 @@ from models.subject import Subject
 
 @pytest.mark.asyncio
 async def test_get_progress(default_client: httpx.AsyncClient,
+                            admin_client: httpx.AsyncClient,
                             create_empty_experiment):
     url = f'/progress/'
 
-    response = await default_client.get(url, auth=('admin', 'admin'))
+    response = await admin_client.get(url)
     assert response.status_code == 200
 
     # Clean up resources
