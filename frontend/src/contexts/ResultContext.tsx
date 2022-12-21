4a2f0483ce3274407bb28d89da48f33e9241723f
vagechirkov <vagechirkov@gmail.com>
2022-12-21 12:06:42 +0100

    refactor contexts

--- frontend/src/contexts/ResultContext.tsx
+++ frontend/src/contexts/ResultContext.tsx
@@ -1,6 +1,7 @@
 import React, {createContext, useEffect, useState} from "react";
 import {Advisor, PostSurvey, Solution, WrittenStrategy} from "../apis/apiTypes";
 
+const LOCAL_STORAGE_KEY = 'resultData';
 
 export type ResultContextType = {
     result: Solution | Advisor | WrittenStrategy | PostSurvey;
@@ -12,15 +13,13 @@ const ResultContext = createContext<ResultContextType | null>(null);
 
 const ResultContextProvider = ({children}: any) => {
     const [result, setResult] = useState<Solution | Advisor | WrittenStrategy | PostSurvey | null>(
-        JSON.parse(localStorage.getItem('trialResults')));
+        JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
 
     useEffect(() => {
-        localStorage.setItem('trialResults', JSON.stringify(result));
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(result));
     }, [result]);
 
-    const updateResult = (newResult: Solution | Advisor | WrittenStrategy | PostSurvey) => {
-        setResult(newResult);
-    }
+    const updateResult = (newResult: Solution | Advisor | WrittenStrategy | PostSurvey) => setResult(newResult)
 
     return (
         <ResultContext.Provider value={{result, updateResult}}>
--- frontend/src/contexts/TrialContext.tsx
+++ frontend/src/contexts/TrialContext.tsx
@@ -1,19 +1,29 @@
 import React, {createContext, useEffect, useState} from "react";
 import {Trial} from "../apis/apiTypes";
 
+const LOCAL_STORAGE_KEY = 'trialData';
 
-export const TrialContext = createContext<Trial | null>(null);
+export type TrialContextType = {
+    trial: Trial | null;
+    updateTrial: (newTrial: Trial) => void;
+};
+
+
+const TrialContext = createContext<TrialContextType | null>(null);
 
 
 const TrialContextProvider = ({children}: any) => {
-    const [trial, setTrial] = useState<Trial | null>(JSON.parse(localStorage.getItem('trialData')));
+    const [trial, setTrial] = useState<Trial | null>(
+        JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
 
     useEffect(() => {
-        localStorage.setItem('trialData', JSON.stringify(trial));
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(trial));
     }, [trial]);
 
+    const updateTrial = (newTrial: Trial) => setTrial(newTrial)
+
     return (
-        <TrialContext.Provider value={trial}>
+        <TrialContext.Provider value={{trial, updateTrial}}>
             {children}
         </TrialContext.Provider>
     );
