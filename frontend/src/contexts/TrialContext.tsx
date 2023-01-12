4237adff58b8859b90df7b4269b0064f46af2b99
vagechirkov <vagechirkov@gmail.com>
2023-01-12 09:35:40 +0100

    extend trial context

--- frontend/src/contexts/TrialContext.tsx
+++ frontend/src/contexts/TrialContext.tsx
@@ -1,14 +1,25 @@
-import React, {createContext, useEffect, useState} from "react";
+import React, {createContext, useContext, useEffect, useState} from "react";
 import {Advisor, PostSurvey, Solution, Trial, WrittenStrategy} from "../apis/apiTypes";
 
 const LOCAL_STORAGE_TRIAL_KEY = 'trialData';
 const LOCAL_STORAGE_RESULT_KEY = 'resultData';
+const LOCAL_STORAGE_NETWORK_STATE_KEY = 'networkState';
+
+export type NetworkState = {
+    step: number;
+    points: number;
+    moves: number[];
+    time: number;
+    currentNode: number;
+}
 
 export type TrialContextType = {
     trial: Trial | null;
     result: Solution | Advisor | WrittenStrategy | PostSurvey;
+    networkState: NetworkState | null;
     updateTrial: (newTrial: Trial) => void;
     updateResult: (newResult: Solution | Advisor | WrittenStrategy | PostSurvey) => void;
+    updateNetworkState: (newNetworkState: NetworkState) => void;
 };
 
 
@@ -20,6 +31,8 @@ const TrialContextProvider = ({children}: any) => {
         JSON.parse(localStorage.getItem(LOCAL_STORAGE_TRIAL_KEY)));
     const [result, setResult] = useState<Solution | Advisor | WrittenStrategy | PostSurvey | null>(
         JSON.parse(localStorage.getItem(LOCAL_STORAGE_RESULT_KEY)));
+    const [networkState, setNetworkState] = useState<NetworkState | null>(
+        JSON.parse(localStorage.getItem(LOCAL_STORAGE_NETWORK_STATE_KEY)));
 
     useEffect(() => {
         localStorage.setItem(LOCAL_STORAGE_TRIAL_KEY, JSON.stringify(trial));
@@ -33,11 +46,15 @@ const TrialContextProvider = ({children}: any) => {
 
     const updateResult = (newResult: Solution | Advisor | WrittenStrategy | PostSurvey) => setResult(newResult)
 
+    const updateNetworkState = (newNetworkState: NetworkState) => setNetworkState(newNetworkState)
+
     return (
-        <TrialContext.Provider value={{trial, result, updateTrial, updateResult}}>
+        <TrialContext.Provider value={{trial, result, networkState, updateTrial, updateResult, updateNetworkState}}>
             {children}
         </TrialContext.Provider>
     );
 };
 
 export default TrialContextProvider;
+
+export const useTrialContext = () => useContext(TrialContext);
