29c19927aac2f62b933466d3e67a6a3e31fbbb42
vagechirkov <vagechirkov@gmail.com>
2023-01-13 16:53:04 +0100

    Implement network context in the network trial

---
+++ frontend/src/components/Trials/NetworkTrial/NetworkTrial.tsx
@@ -0,0 +1,65 @@
+import React, {FC} from "react";
+import {NETWORK_ACTIONS, useNetworkContext} from "../../../contexts/NetworkContext";
+import {Divider, Grid} from "@mui/material";
+import StaticNetwork, {StaticNetworkEdgeInterface, StaticNetworkNodeInterface} from "../../Network/StaticNetwork/StaticNetwork";
+
+interface NetworkTrialInterface {
+    nodes: StaticNetworkNodeInterface[];
+    edges: StaticNetworkEdgeInterface[];
+}
+
+const NetworkTrial: FC<NetworkTrialInterface> = ({nodes, edges}) => {
+    const {networkState, networkDispatcher} = useNetworkContext();
+
+    const NodeClickHandler = (nodeIndex: number) => {
+        networkDispatcher({type: NETWORK_ACTIONS.NEXT_NODE, payload: {edges, nodeIndex}});
+    }
+
+    return (
+        <Grid container sx={{margin: 'auto', width: '85%'}} justifyContent="space-around">
+            <Grid item sx={{p: 1}} xs={3}>
+                <Grid container direction="column">
+                    <Grid item xs={4}>
+                        {/*{showTimer &&*/}
+                        {/*    <Timer*/}
+                        {/*        time={25}*/}
+                        {/*        invisibleTime={5} // 5 seconds before the timer starts*/}
+                        {/*        // OnTimeEndHandler={() => setIsTimerDone(true)}*/}
+                        {/*    />*/}
+                        {/*}*/}
+                    </Grid>
+                    <Grid item xs={8}>
+                        {/*<PlayerInformation*/}
+                        {/*    id={1}*/}
+                        {/*    step={step}*/}
+                        {/*    cumulativePoints={points}*/}
+                        {/*    showComment={false}*/}
+                        {/*/>*/}
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={7}>
+                <Grid container direction="row" justifyContent="space-around">
+                    <Grid item>
+                        <StaticNetwork
+                            edges={edges}
+                            nodes={nodes}
+                            currentNodeId={networkState.currentNode}
+                            possibleMoves={networkState.possibleMoves}
+                            onNodeClickHandler={networkState.isNetworkDisabled ? null : NodeClickHandler}
+                        />
+                        <Divider variant="middle" light/>
+                    </Grid>
+                    <Grid item sx={{marginTop: '10px'}}>
+                        {/*{showLinearSolution &&*/}
+                        {/*    <LinearSolution/>*/}
+                        {/*}*/}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+}
+
+
+export default NetworkTrial;
--- frontend/src/contexts/NetworkContext.tsx
+++ frontend/src/contexts/NetworkContext.tsx
@@ -1,6 +1,14 @@
-import React, {createContext, useContext, useEffect, useState} from "react";
+import React, {createContext, useContext, useEffect, useReducer} from "react";
+import {StaticNetworkEdgeInterface} from "../components/Network/StaticNetwork/StaticNetwork";
 
 const LOCAL_STORAGE_NETWORK_STATE_KEY = 'networkState';
+
+export const NETWORK_ACTIONS = {
+    NEXT_NODE: 'nextNode',
+    TIMER_DONE: 'timerDone',
+    DISABLE: 'disable',
+}
+
 
 export type NetworkState = {
     step: number;
@@ -8,12 +16,13 @@ export type NetworkState = {
     moves: number[];
     time: number;
     currentNode: number;
-    tutorialId?: number;
+    possibleMoves: number[];
+    isNetworkDisabled: boolean;
 }
 
 export type NetworkContextType = {
     networkState: NetworkState | null;
-    updateNetworkState: (newNetworkState: NetworkState) => void;
+    networkDispatcher: (action: any) => void;
 };
 
 
@@ -21,24 +30,58 @@ export const NetworkContext = createContext<NetworkContextType | null>(null);
 
 
 const NetworkContextProvider = ({children}: any) => {
-    const [networkState, setNetworkState] = useState<NetworkState | null>(
-        JSON.parse(localStorage.getItem(LOCAL_STORAGE_NETWORK_STATE_KEY))
-    );
+    const [networkState, networkDispatcher] = useReducer(reducer, {
+        step: 0,
+        points: 0,
+        moves: [],
+        time: 0,
+        currentNode: 0,
+        possibleMoves: [],
+        isNetworkDisabled: false,
+    });
+    // JSON.parse(localStorage.getItem(LOCAL_STORAGE_NETWORK_STATE_KEY))
 
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_NETWORK_STATE_KEY, JSON.stringify(networkState));
-    }, [networkState]);
-
-
-    const updateNetworkState = (newNetworkState: NetworkState) => setNetworkState(newNetworkState)
+    // useEffect(() => {
+    //     localStorage.setItem(LOCAL_STORAGE_NETWORK_STATE_KEY, JSON.stringify(networkState));
+    // }, [networkState]);
 
 
     return (
-        <NetworkContext.Provider value={{networkState, updateNetworkState}}>
+        <NetworkContext.Provider value={{networkState, networkDispatcher}}>
             {children}
         </NetworkContext.Provider>
     );
 };
+
+const reducer = (state: NetworkState, action: any) => {
+    switch (action.type) {
+        case NETWORK_ACTIONS.NEXT_NODE:
+            const possibleMoves = selectPossibleMoves(action.payload.edges, state.currentNode);
+
+            if (possibleMoves.includes(action.payload.nodeIdx)) {
+                return {
+                    ...state,
+                    currentNode: action.payload.nodeIdx,
+                    moves: state.moves.concat([action.payload.nodeIdx]),
+                    points: state.points + action.payload.reward,
+                    step: state.step + 1,
+                }
+            } else return state;
+        case NETWORK_ACTIONS.DISABLE:
+            return {
+                ...state,
+                isNetworkDisabled: true,
+            }
+        default:
+            return state;
+    }
+}
+
+const selectPossibleMoves = (allEdges: StaticNetworkEdgeInterface[], currentNodeId: number) => {
+    return allEdges
+        .filter((edge) => edge.source_num === currentNodeId)
+        .map((edge) => edge.target_num);
+}
 
 export default NetworkContextProvider;
 
