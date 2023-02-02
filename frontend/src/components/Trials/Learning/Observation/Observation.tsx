2b5aaa943738b41869e8c44faf700611bb223e7e
vagechirkov <vagechirkov@gmail.com>
2023-02-02 10:31:10 +0100

    add observation component

---
+++ frontend/src/components/Trials/Learning/Observation/Observation.tsx
@@ -0,0 +1,45 @@
+import React, {FC, useEffect} from "react";
+import NetworkTrial from "../../NetworkTrial";
+import useNetworkContext from "../../../../contexts/NetworkContext";
+import {NETWORK_ACTIONS} from "../../../../reducers/NetworkReducer";
+
+
+interface IObservation {
+    solution: number[];
+    /** Delay in ms between each played move. Default is 2000ms. */
+    delayBetweenMoves?: number;
+    /** Start the animation from the parent component. Default is true. */
+    playAnimation?: boolean;
+}
+
+const Observation: FC<IObservation> = (props) => {
+    const {networkState, networkDispatcher} = useNetworkContext();
+    const {solution, playAnimation = true, delayBetweenMoves = 2000} = props;
+
+    useEffect(() => {
+        if (playAnimation) {
+            setTimeout(() => {
+                if (networkState.step < solution.length - 1) {
+                    networkDispatcher({
+                            type: NETWORK_ACTIONS.NEXT_NODE,
+                            payload: {nodeIdx: solution[networkState.step + 1]}
+                        }
+                    );
+                } else {
+
+                }
+            }, delayBetweenMoves);
+        }
+
+    }, [])
+
+
+    return (
+        <>
+            <NetworkTrial/>
+        </>
+    );
+
+}
+
+export default Observation;
