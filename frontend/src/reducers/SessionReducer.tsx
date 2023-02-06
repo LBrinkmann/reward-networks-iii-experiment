50dd5910f8f6bfe07fef23cb49078de52c15ee5d
vagechirkov <vagechirkov@gmail.com>
2023-02-06 16:30:31 +0100

    update example id

--- frontend/src/reducers/SessionReducer.tsx
+++ frontend/src/reducers/SessionReducer.tsx
@@ -1,4 +1,5 @@
 import {SessionState} from "../contexts/SessionContext";
+import {TRIAL_TYPE} from "../components/Trials/ExperimentTrial";
 
 
 export const SESSION_ACTIONS = {
@@ -11,13 +12,17 @@ export const SESSION_ACTIONS = {
 const sessionReducer = (state: SessionState, action: any) => {
     switch (action.type) {
         case SESSION_ACTIONS.SET_CURRENT_TRIAL:
+            let selectedAdvisorExampleId = action.payload.currentTrialType === TRIAL_TYPE.SOCIAL_LEARNING_SELECTION ?
+                0 : state.selectedAdvisorExampleId;
+            if (action.payload.currentTrialType === TRIAL_TYPE.OBSERVATION) selectedAdvisorExampleId++;
+
             return {
                 ...state,
                 currentTrialType: action.payload.currentTrialType,
                 currentTrialId: action.payload.currentTrialId,
                 // show tutorial for social learning selection and observation trials
                 showTutorialInCurrentTrial: action.payload.currentTrialId < 8,
-                isTrialFinished: false,
+                selectedAdvisorExampleId: selectedAdvisorExampleId,
             }
         case SESSION_ACTIONS.SET_ADVISORS:
             return {
