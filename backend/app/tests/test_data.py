ded189d6e3ae3fbcb062638a2cbe5c83fcca3646
vagechirkov <vagechirkov@gmail.com>
2023-02-28 10:41:07 +0100

    🐛fix new network data for testing

--- backend/app/tests/test_data.py
+++ backend/app/tests/test_data.py
@@ -6,17 +6,16 @@ from utils.utils import estimate_solution_score
 
 
 def test_network_solutions():
-    networks_json = json.load(open(Path('data') / 'train_viz.json'))
+    networks_json = json.load(open(Path('data') / 'networks.json'))
     networks = [Network.parse_obj(n) for n in networks_json]
-    solutions = json.load(
-        open(Path('data') / 'solution_moves_take_first_loss_viz.json'))
+    solutions = json.load(open(Path('data') / 'solutions_loss.json'))
 
     ids = [s['network_id'] for s in solutions]
 
     for n in networks:
         assert n.network_id in ids
-        moves = [s for s in solutions if s['network_id'] == n.network_id][0][
-            'moves']
+        moves = [s for s in solutions if s['network_id'] == n.network_id][0]['moves']
+        moves[0] = n.starting_node
 
         # check that all moves are valid
         assert estimate_solution_score(n, moves) != -100_000
