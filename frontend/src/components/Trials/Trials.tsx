7d3321497a8c989a280a2ad74f01be5267945697
vagechirkov <vagechirkov@gmail.com>
2023-05-25 18:20:47 +0200

    call written strategy "your strategy"

--- frontend/src/components/Trials/Trials.tsx
+++ frontend/src/components/Trials/Trials.tsx
@@ -319,7 +319,7 @@ export const WrittenStrategyTrial: FC<ITrial> = (props) => {
 
     return (
         <>
-            <Header title={'Written Strategy'}/>
+            <Header title={'Your Strategy'}/>
             <WrittenStrategy endTrial={props.endTrial} type={sessionState.currentTrialId < 10 ? "start" : "end"}/>
         </>
     );
